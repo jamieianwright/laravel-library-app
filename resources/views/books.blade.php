189f6311d8454b950a70315deaabc276d0d063c0
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2019-03-31 22:46:05 +0100

    Add update book modal to books view

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -50,6 +50,60 @@
     <div class='container'>
       <h1 class='title mt-4'>Books</h1>
 
+      <div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Update Book Details</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form class='' id='updateBookForm' method='PUT' action='/books'>
+                {{ csrf_field() }}
+                <div class='row'>
+                  <div class="form-group col-md-6 col-sm-12">
+                    <label for="bookTitle">Title</label>
+                    <input type="text" class="form-control" id='updateTitle' name='updateTitle' id="bookTitle" placeholder="Enter Book Title">
+                  </div>
+                  <div class="form-group col-md-6 col-sm-12">
+                    <label for="bookReleaseDate">Release Date</label>
+                    <input type="date" class="form-control" id='updateReleaseDate' name='updateReleaseDate' id="bookReleaseDate" placeholder="Pick a release date">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="description">Description</label>
+                  <textarea type="text" class="form-control" id='updateDescription' name='updateDescription' placeholder="Enter Book Description"></textarea>
+                </div>
+                <div class='row'>
+                  <div class="form-group col-md-6 col-sm-12">
+                      <label for="bookAuthor">Author</label>
+                      <select class="form-control" name='updateAuthor' id='updateAuthor'>
+                          @foreach ($authors as $author)
+                              <option value="{{$author->id}}">{{$author->name}}</option>
+                          @endforeach
+                      </select>
+                  </div>
+                  <div class="form-group col-md-6 col-sm-12">
+                    <label for="bookPublisher">Publisher</label>
+                    <select class="form-control" name='updatePublisher' id="updatePublisher">
+                        @foreach ($publishers as $publisher)
+                            <option value="{{$publisher->id}}">{{$publisher->name}}</option>
+                        @endforeach
+                    </select>
+                  </div>
+                </div>              
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Save changes</button>
+            </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <button class="btn btn-primary" id='addbtn' type="button" data-toggle="collapse" data-target="#addBookForm" aria-expanded="false" aria-controls="collapse">Add New Book</button>
 
       <div class='my-4'>
@@ -65,30 +119,29 @@
                   <input type="date" class="form-control" name='bookReleaseDate' id="bookReleaseDate" placeholder="Pick a release date">
                 </div>
           </div>
-          
           <div class="form-group">
             <label for="description">Description</label>
             <textarea type="text" class="form-control" name='bookDescription' id="bookDescription" placeholder="Enter Book Description"></textarea>
           </div>
           <div class='row'>
-              <div class="form-group col-md-6 col-sm-12">
-                  <label for="bookAuthor">Author</label>
-                  <select class="form-control" name='bookAuthor' id="bookAuthor">
-                    <option value="" disabled selected hidden>Pick an Author</option>
-                      @foreach ($authors as $author)
-                          <option value="{{$author->id}}">{{$author->name}}</option>
-                      @endforeach
-                  </select>
-              </div>
-              <div class="form-group col-md-6 col-sm-12">
-                <label for="bookPublisher">Publisher</label>
-                <select class="form-control" name='bookPublisher' id="bookPublisher">
-                  <option value="" disabled selected hidden>Pick an Publisher</option>
-                    @foreach ($publishers as $publisher)
-                        <option value="{{$publisher->id}}">{{$publisher->name}}</option>
+            <div class="form-group col-md-6 col-sm-12">
+                <label for="bookAuthor">Author</label>
+                <select class="form-control" name='bookAuthor' id="bookAuthor">
+                  <option value="" disabled selected hidden>Pick an Author</option>
+                    @foreach ($authors as $author)
+                        <option value="{{$author->id}}">{{$author->name}}</option>
                     @endforeach
                 </select>
-              </div>
+            </div>
+            <div class="form-group col-md-6 col-sm-12">
+              <label for="bookPublisher">Publisher</label>
+              <select class="form-control" name='bookPublisher' id="bookPublisher">
+                <option value="" disabled selected hidden>Pick an Publisher</option>
+                  @foreach ($publishers as $publisher)
+                      <option value="{{$publisher->id}}">{{$publisher->name}}</option>
+                  @endforeach
+              </select>
+            </div>
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
@@ -107,12 +160,12 @@
         </thead>
         <tbody>
             @foreach ($books as $book)
-                <tr>
-                    <th scope="row">{{$book->title}}</th>
-                    <td>{{$book->description}}</td>
-                    <td>{{$book->release_date}}</td>
-                    <td>{{$book->author->name}}</td>
-                    <td>{{$book->publisher->name}}</td>
+                <tr data-id='{{$book->id}}' class='book'>
+                    <th scope="row" class='btn btn-outline-secondary bookTitle' title='{{$book->title}}' data-toggle="modal" data-target="#updateModal">{{$book->title}}</th>
+                    <td class='bookDescription'>{{$book->description}}</td>
+                    <td class='bookReleaseDate'>{{$book->release_date}}</td>
+                    <td class='bookAuthor'>{{$book->author->name}}</td>
+                    <td class='bookPublisher'>{{$book->publisher->name}}</td>
                     <td>
                         @foreach ($book->genres as $genre)
                             <span class="badge badge-primary">{{$genre->name}}</span>
@@ -132,6 +185,26 @@
               return text === "Add New Book" ? "Close" : "Add New Book";
           })
         });
+
+        $(".btn").click(function(e){
+          var id = $(this).parent('.book').data('id')
+          $(this).text(function(i, text){
+            $('#updateTitle').val(text)
+          })
+          $(this).siblings('.bookReleaseDate').text(function(i, text){
+            $('#updateReleaseDate').val(text)
+          })
+          $(this).siblings('.bookDescription').text(function(i, text){
+            $('#updateDescription').val(text)
+          })
+          $(this).siblings('.bookAuthor').text(function(i, text){
+            $("#updateAuthor option:contains(" + text + ")").attr('selected','selected');
+          }).change()
+          $(this).siblings('.bookPublisher').text(function(i, text){
+            $("#updatePublisher option:contains(" + text + ")").attr('selected','selected');
+          })
+          $('#updateBookForm').attr('action', '/books/' + id)
+        })
     });
   </script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
